2177dc15973f1252ca6f85c3d28dd72a74761802
JannisHeyer <jbr.heyer@outlook.de>
2023-11-16 20:29:31 +0100

    Change btn hover

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -1,9 +1,11 @@
 <script></script>
 
 <template>
-  <button class="bg-bg w-28 h-8 flex items-center justify-center border-2 border-green-500 hover:glow hover:bg-highlight/10">
+  <button
+    class="bg-bg w-28 h-8 flex items-center justify-center border-2 border-green-500 hover:glow hover:bg-highlight/10"
+  >
     <span class="text-primary text-center">
-      <slot></slot>
+      <slot></slot><span class="text-2xl">.</span>
     </span>
   </button>
 </template>
@@ -13,21 +15,13 @@
   @apply transition-all duration-200 ease-in-out;
 }
 
-.hover\:glow:hover {
-  box-shadow: 0 0 7px var(--color-primary), 0 0 8px var(--color-primary);
-}
-/* .hexagon-button {
-  background-color: black;
-  width: 7em; 
-  height: 2em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 2px solid #00ff00;
+.hover\:glow:hover .text-2xl {
+  animation: blinker 1s linear infinite;
 }
 
-.hexagon-inner {
-  color: #0f0; 
-  text-align: center;
-} */
+@keyframes blinker {
+  50% {
+    opacity: 0;
+  }
+}
 </style>
