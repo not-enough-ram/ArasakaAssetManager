34d8ba52a65c7d4349ee9925f3029102506442f4
Jonathan Heyer <jonathan.heyer@tallence.com>
2023-11-17 12:02:27 +0100

    Added CreateAssetForm

---
+++ components/CreateAssetForm.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  description: String,
+  image: File,
+});
+
+const formData = ref({
+  title: props.title || "",
+  description: props.description || "",
+  image: null as File | null,
+});
+
+function handleFileUpload(event: Event) {
+  const file = (event.target as HTMLInputElement).files?.[0];
+  formData.value.image = file || null;
+}
+
+function submitForm() {
+  // Process form data here
+  console.log(formData.value);
+}
+</script>
+
+<template>
+  <div class="w-1/3 border-2 border-primary p-5 max-w-360">
+    <form @submit.prevent="submitForm" class="space-y-4">
+      <BaseInput type="text" v-model="formData.title" placeholder="Title..." />
+      <BaseTextarea
+        v-model="formData.description"
+        placeholder="Description..."
+        :maxHeight="'350px'"
+        :maxWidth="'600px'"
+      />
+      <div class="w-full mb-12 cursor-pointer">
+        <input
+          type="file"
+          @change="handleFileUpload"
+          class="text-sm file:mr-4 file:py-2 file:px-4 file:border-2 file:border-primary file:text-sm file:font-semibold file:bg-bg file:text-primary"
+        />
+      </div>
+      <div class="w-full flex justify-center items-center">
+        <span class="icon-floppy-disk mr-4 text-2xl"></span>
+        <BaseButton type="submit">Submit</BaseButton>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+#file-upload-button {
+  cursor: pointer;
+}
+</style>
--- pages/CreateContent/index.vue
+++ pages/CreateContent/index.vue
@@ -10,6 +10,12 @@
       </template>
     </Typewriter>
     <CreateGameForm />
+    <Typewriter :text="'Add an asset...'" :typingDelay="150" class="mb-5 mt-5">
+      <template #default="{ animatedText }">
+        <h1 class="text-primary text-4xl">{{ animatedText }}</h1>
+      </template>
+    </Typewriter>
+    <CreateAssetForm />
   </section>
 </template>
 
