eeaa8f30d9f8a6045bf0d397a27f76f0fa431167
JannisHeyer <jbr.heyer@outlook.de>
2023-11-17 16:47:55 +0100

    WIP: use composables to handle state across components

--- pages/Profile/index.vue
+++ pages/Profile/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { fetchData } from '../../services/ApiService';
-
+import useLoadingState from '@/composables/useLoadingState';
+const { isLoading, showLoading, hideLoading } = useLoadingState();
 const loadingProgress = ref(0);
 
-async function loadData() {
-  // Call fetchData with the test flag set to true
+const loadData = async () => {
+  showLoading();
   await fetchData(
     '/test-endpoint',
     (progress) => {
@@ -13,14 +14,15 @@ async function loadData() {
     },
     true
   );
-}
+  hideLoading();
+};
 
 onMounted(loadData);
 </script>
 
 <template>
   <div>
-    <LoadingIndicator :loading-percentage="loadingProgress" />
+    <LoadingIndicator :loading-percentage="loadingProgress" v-if="isLoading" />
   </div>
 </template>
 
